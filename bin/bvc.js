bbe51e329f7b1a3b736c623f44ff1ba45c0a2d1a
Lviffy <contact.rohan.here@gmail.com>
2025-09-05 20:08:40 +0530

    cli

--- bin/bvc.js
+++ bin/bvc.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
 const { Command } = require('commander');
+const CLIUtils = require('../lib/utils/cli');
+
+// Import commands
 const initCommand = require('../lib/commands/init');
 const addCommand = require('../lib/commands/add');
 const commitCommand = require('../lib/commands/commit');
@@ -14,12 +17,34 @@ const checkpointCommand = require('../lib/commands/checkpoint');
 
 const program = new Command();
 
+// Global error handler
+process.on('uncaughtException', (error) => {
+  CLIUtils.handleError(error, 'Uncaught Exception');
+});
+
+process.on('unhandledRejection', (error) => {
+  CLIUtils.handleError(error, 'Unhandled Promise Rejection');
+});
+
 program
   .name('bvc')
-  .description('Blockchain Version Control CLI')
-  .version('1.0.0');
+  .description(CLIUtils.colors.primary('🔗 Blockchain Version Control - Decentralized Git powered by blockchain and IPFS'))
+  .version('1.0.0')
+  .option('-v, --verbose', 'Enable verbose output')
+  .option('--debug', 'Enable debug mode')
+  .hook('preAction', (thisCommand, actionCommand) => {
+    // Set debug mode if requested
+    if (program.opts().debug) {
+      process.env.DEBUG = 'true';
+    }
+    
+    // Show banner for main commands (not for help)
+    if (actionCommand.name() !== 'help' && !actionCommand.parent) {
+      CLIUtils.showBanner();
+    }
+  });
 
-// Register commands
+// Register commands with enhanced descriptions
 program.addCommand(initCommand);
 program.addCommand(addCommand);
 program.addCommand(commitCommand);
@@ -30,5 +55,43 @@ program.addCommand(pullCommand);
 program.addCommand(cloneCommand);
 program.addCommand(configCommand);
 program.addCommand(checkpointCommand);
+
+// Custom help command
+program
+  .command('help')
+  .description('Display help information')
+  .argument('[command]', 'Command to get help for')
+  .action((command) => {
+    if (!command) {
+      CLIUtils.showBanner();
+      program.help();
+    } else {
+      const cmd = program.commands.find(c => c.name() === command);
+      if (cmd) {
+        cmd.help();
+      } else {
+        CLIUtils.error(`Unknown command: ${command}`);
+        console.log('\nAvailable commands:');
+        program.commands.forEach(c => {
+          if (c.name() !== 'help') {
+            console.log(CLIUtils.colors.primary(`  ${c.name()}`));
+          }
+        });
+      }
+    }
+  });
+
+// Enhanced error handling for unknown commands
+program.on('command:*', () => {
+  CLIUtils.error(`Unknown command: ${program.args.join(' ')}`);
+  CLIUtils.info('Run "bvc help" to see available commands');
+  process.exit(1);
+});
+
+// Show help if no command provided
+if (!process.argv.slice(2).length) {
+  CLIUtils.showBanner();
+  program.help();
+}
 
 program.parse();
